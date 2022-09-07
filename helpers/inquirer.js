58ab24708e4731a1030c3a0007fcf5c81a2fe253
Jymy Escobar <jdany.escobar@gmail.com>
2022-09-06 22:58:42 -0600

    menu

--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -8,33 +8,17 @@ const preguntas = [
     message: 'Que deseas hacer?',
     choices: [
         {
-            value: '1',
-            name: `${'1.'.green} Crear tarea`
+            value: 1,
+            name: `${'1.'.green} Buscar ciudad`
         },
         {
-            value: '2',
-            name: `${'2.'.green} Listar tarea`
+            value: 2,
+            name: `${'2.'.green} Historial`
         },
         {
-            value: '3',
-            name: `${'3.'.green} Listar tareas completadas`
-        },
-        {
-            value: '4',
-            name: `${'4.'.green} Listar tareas pendientes`
-        },
-        {
-            value: '5',
-            name: `${'5.'.green} Completar tare(s)`
-        },
-        {
-            value: '6',
-            name: `${'6.'.green} Borarar tarea`
-        },
-        {
-            value: '0',
+            value: 0,
             name: `${'0.'.green} Salir`
-        },
+        }
     ]
   }  
 ];
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const { leerInput } = require("./helpers/inquirer")
+const { inquirerMenu,pausa,leerInput } = require("./helpers/inquirer")
 
 
 const main = async() => {
-    const text = await leerInput('Hola:');
+    let opt;
 
-    console.log(text);
+    do {
+        opt = await inquirerMenu();
+
+        switch (opt) {
+            case 1:
+                
+                break;
+            case 2:
+            
+                break;
+        }
+
+        if(opt !== 0) await pausa();
+
+    } while (opt != 0);
 }
 
 
